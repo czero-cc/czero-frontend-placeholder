2b77781d29b1e2b2ea40058f1c8dbda0f937c450
arahangua <arahangua@gmail.com>
2025-02-20 14:09:41 +0100

    added:netlify configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
+
 
 declare module "@remix-run/node" {
   interface Future {
@@ -19,6 +21,7 @@ export default defineConfig({
         v3_lazyRouteDiscovery: true,
       },
     }),
+    netlify
     tsconfigPaths(),
   ],
 });
