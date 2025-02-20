a5ecbad117dc687a8ca5e4dce871918e82dd7a96
arahangua <arahangua@gmail.com>
2025-02-20 13:51:19 +0100

    feat: Update homepage logo, mission, and contact email

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -18,19 +18,19 @@ export default function Index() {
       <div className="flex flex-col items-center gap-12">
         <header>
           <img
-            src="/logo.png"
-            alt="Your Logo"
-            className="block h-32 w-auto"
+            src="/c0-logo.svg"
+            alt="Czero Logo"
+            className="block h-80 w-auto"
           />
         </header>
         <main className="text-center">
           <p className="text-xl font-light">
-            Your mission statement here.
+          Enable your AI-driven Software Development Pipelines
           </p>
         </main>
         <footer>
           <p className="text-gray-700">
-            Contact us: <a href="mailto:contact@example.com" className="hover:underline">contact@example.com</a>
+            Contact us: <a href="mailto:contact@example.com" className="hover:underline">info@czero.cc</a>
           </p>
         </footer>
       </div>
